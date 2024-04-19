18769abc4c46abecac32edea1b0107c0c407fa38
VadimKorobec <_s_e_t__@ukr.net>
2024-04-19 11:44:56 +0300

    add some code

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,3 +1,4 @@
+import { useDispatch, useSelector } from "react-redux";
 import {
   heroesFetching,
   heroesFetched,
@@ -5,6 +6,8 @@ import {
 } from "../../actions";
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from "../spinner/Spinner";
+import { useHttp } from "../../hooks/http.hook";
+import { useEffect } from "react";
 
 // Задача для этого компонента:
 // При клике на "крестик" идет удаление персонажа из общего состояния
@@ -12,24 +15,24 @@ import Spinner from "../spinner/Spinner";
 // Удаление идет и с json файла при помощи метода DELETE
 
 const HeroesList = () => {
-  // const { heroes, heroesLoadingStatus } = useSelector((state) => state);
-  // const dispatch = useDispatch();
-  // const { request } = useHttp();
+  const { heroes, heroesLoadingStatus } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const { request } = useHttp();
 
-  // useEffect(() => {
-  //   dispatch(heroesFetching());
-  //   request("http://localhost:3001/heroes")
-  //     .then((data) => dispatch(heroesFetched(data)))
-  //     .catch(() => dispatch(heroesFetchingError()));
+  useEffect(() => {
+    dispatch(heroesFetching());
+    request("http://localhost:3001/heroes")
+      .then((data) => dispatch(heroesFetched(data)))
+      .catch(() => dispatch(heroesFetchingError()));
 
-  //   // eslint-disable-next-line
-  // }, []);
+    // eslint-disable-next-line
+  }, []);
 
-  // if (heroesLoadingStatus === "loading") {
-  //   return <Spinner />;
-  // } else if (heroesLoadingStatus === "error") {
-  //   return <h5 className="text-center mt-5">Ошибка загрузки</h5>;
-  // }
+  if (heroesLoadingStatus === "loading") {
+    return <Spinner />;
+  } else if (heroesLoadingStatus === "error") {
+    return <h5 className="text-center mt-5">Ошибка загрузки</h5>;
+  }
 
   const renderHeroesList = (arr) => {
     if (arr.length === 0) {
@@ -41,8 +44,8 @@ const HeroesList = () => {
     });
   };
 
-  // const elements = renderHeroesList(heroes);
-  // return <ul>{elements}</ul>;
+  const elements = renderHeroesList(heroes);
+  return <ul>{elements}</ul>;
 };
 
 export default HeroesList;
